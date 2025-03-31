39c5744cae0186bc085667a2dc594303743db3d7
zitaharry <harrymuigua@gmail.com>
2025-03-31 09:48:59 +0300

    Implement Interview Card UI and Firebase setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,56 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import InterviewCard from "@/components/InterviewCard";
+import { dummyInterviews } from "@/constants";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+
+          {/*<p>You haven't taken any interviews yet.</p>*/}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 export default HomePage;
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,6 +1,82 @@
 import React from "react";
+import dayjs from "dayjs";
+import Image from "next/image";
+import { getRandomInterviewCover } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import DisplayTechIcons from "@/components/DisplayTechIcons";
+import Link from "next/link";
 
-const InterviewCard = () => {
-  return <div>InterviewCard</div>;
+const InterviewCard = ({
+  interviewId,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now(),
+  ).format("MMM D, YYYY");
+
+  return (
+    <div className="card-border w-[360px] max-sm:w-full min-h-96">
+      <div className="card-interview">
+        <div>
+          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+            <p className="badge-text">{normalizedType}</p>
+          </div>
+
+          <Image
+            src={getRandomInterviewCover()}
+            alt="cover"
+            width={90}
+            height={90}
+            className="rounded-full object-fit size-[90px]"
+          />
+
+          <h3 className="mt-5 capitalize">{role} Interview</h3>
+
+          <div className="flex flex-row gap-5 mt-3">
+            <div className="flex flex-row gap-2">
+              <Image
+                src="/calendar.svg"
+                alt="calendar"
+                width={22}
+                height={22}
+              />
+              <p>{formattedDate}</p>
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Image src="/star.svg" alt="star" width={22} height={22} />
+              <p>{feedback?.totalScore || "---"}/100</p>
+            </div>
+          </div>
+
+          <p className="line-clamp-2 mt-5">
+            {feedback?.finalAssessment ||
+              "You haven't taken this interview yet. Take it now to improve your skills."}
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <DisplayTechIcons techStack={techstack} />
+
+          <Button className="btn-primary">
+            <Link
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
+            >
+              {feedback ? "Check Feedback" : "View Interview"}
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default InterviewCard;
